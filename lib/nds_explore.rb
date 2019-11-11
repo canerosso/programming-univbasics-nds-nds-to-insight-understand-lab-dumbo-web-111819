44965b6e5b4dae303b11b946997626e076808201
Cane Rosso <astor@usa.com>
2019-11-11 03:54:30 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,6 +10,13 @@ end
 
 def print_first_directors_movie_titles
   # When exploring the data structure using custom methods, print_first_directors_movie_titles loops over Spielberg’s movies and prints out their names with newlines
-  
+   ss_movies = directors_database[0][:movies]
+  index = 0
+
+  while index < ss_movies.length do
+    titles = ss_movies[index][:title]
+    puts titles
+    index +=1
+  end
   
 end
